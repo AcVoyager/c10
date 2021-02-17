6054faa45f0d80448731875d26c6b4458172291c
acvoyager <xcvoyager01@gmail.com>
2021-02-17 14:47:08 -0800

    typewritting effect done; but auto-scroll failed

--- src/components/Para.js
+++ src/components/Para.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Para(props) {
+
+    const [end, setEnd] = useState(1);
+    const [shownText, setShownText] = useState("");
+    const text = props.text;
+    const wordLength = text.length;
+    const symbols = "。？！；";
+
+    const typewrite = () => {
+        let word = text.substring(0, end);
+        setShownText(word);
+        setEnd(end + 1);
+    }
+
+    useEffect(() => {
+        // If there's still word to show
+        if(end <= wordLength) {
+            const itv = end > 1 && symbols.includes(text.charAt(end-2)) ? 600 : 75;
+            const TimerID = setTimeout(() => typewrite(), itv);
+            return () => {
+                clearTimeout(TimerID);
+            }
+        } 
+    })
+
     return (
-        <p style={props.style}>{props.text}</p>
+        <p style={props.style}>{shownText}</p>
     );
 }
 
